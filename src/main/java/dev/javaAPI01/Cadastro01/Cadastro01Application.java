02ef2d19be91211942e0b6998b5474e8f996bffb
Porquin13 <gguga.diass@gmail.com>
2026-01-28 14:48:24 -0300

    first commit

---
+++ src/main/java/dev/javaAPI01/Cadastro01/Cadastro01Application.java
@@ -0,0 +1,13 @@
+package dev.javaAPI01.Cadastro01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cadastro01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cadastro01Application.class, args);
+	}
+
+}
---
+++ src/test/java/dev/javaAPI01/Cadastro01/Cadastro01ApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.javaAPI01.Cadastro01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cadastro01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
